5d1e5e4c4e5a4c51243cb90647b4f98ff7ff8bfe
AAMIIN46 <78336290+AAMIIN46@users.noreply.github.com>
2025-11-25 13:47:36 +0300

    delete old folders

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
 const express = require("express");
 const app = express();
-const pgk = require("./db.js");
 
 //middleware
 app.use(express.json());
-
 
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
-});
-
-app.get("/users", async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM users");
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("DB error");
-  }
 });
 
 app.listen(port, () => {
